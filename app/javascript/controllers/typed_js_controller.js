d330e867ff13152e39bad965bc118773de26ee37
larissa hury <larissa.marquesbaptista@gmail.com>
2022-06-22 21:59:07 +0200

    add javascript controllers and add typedjs

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["Change your life", "Learn to code"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+}
